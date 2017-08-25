74bdca1862d5c1db7717c44d27ec596b205bb6ec
Toon <toon.carmen@student.kdg.be>
2017-08-25 10:38:07 +0200

    frontend recreated

--- trips-frontend/src/app/app.module.ts
+++ trips-frontend/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RegisterComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
